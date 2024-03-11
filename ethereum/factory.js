615804d980b462a68e260036ebeb0c4f4822c9a4
timaevV <timaevvladislav@gmail.com>
2024-03-12 01:30:50 +0600

    feat: web3 and packages

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,5 +1,9 @@
 import web3 from "./web3"
-
 import CampaignFactory from "./build/CampaignFactory.json"
 
-const instance = new web3.eth
+const instance = new web3.eth.Contract(
+    CampaignFactory.abi,
+    "0xd9145CCE52D386f254917e481eB44e9943F39138"
+)
+
+export default instance
